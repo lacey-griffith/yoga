ffa9e2fbbfa954a0445fb1d8e62d09aa172d713f
Lacey <lacey.griffith@g.austincc.edu>
2021-05-19 22:09:41 -0500

    add comment model

--- models/index.js
+++ models/index.js
@@ -1,8 +1,37 @@
+const Comment = require('./Comment');
 const Pose = require('./Pose');
 const User = require('./User');
 
 
 //create associations
 
+//user has many poses
+User.hasMany(Pose, {
+    foreignKey: 'user_id'
+});
 
-module.exports = { Pose, User };
+//user has many comments
+User.hasMany(Comment, {
+    foreignKey: 'user_id'
+})
+
+//comments belong to user
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+})
+
+//comments belong to pose
+Comment.belongsTo(Pose, {
+    foreignKey: 'pose_id'
+})
+
+//poses have many users
+Pose.hasMany(User, {
+    foreignKey: 'pose_id'
+})
+//poses have many comments
+Pose.hasMany(Comment, {
+    foreignKey: 'pose_id'
+})
+
+module.exports = { Pose, User, Comment };
