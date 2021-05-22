d7426e49a54a84a0e611e119aef8064ad6c054af
Lacey <lacey.griffith@g.austincc.edu>
2021-05-21 21:08:26 -0500

    add password hashing

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -2,13 +2,18 @@ const router = require('express').Router();
 const { Comment } = require('../../models');
 
 //get all comments
+router.get()
 
 //get comment by id
+router.get()
 
 //update comment
+router.put()
 
 //create new comment
+router.post()
 
 //delete comment
+router.delete()
 
 module.exports = router;
--- models/User.js
+++ models/User.js
@@ -1,46 +1,45 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
 
 class User extends Model {
-    //add check password function
-    // checkPassword(loginPw) {
-    //     return bcrypt.compareSync(loginPw, this.password)
-    // }
-    //add hooks to model
+  //add check password function
+  // checkPassword(loginPw) {
+  //     return bcrypt.compareSync(loginPw, this.password)
+  // }
+  //add hooks to model
 }
 
-User.init(
-  {
-    id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        primaryKey: true,
-        autoIncrement: true
-      },
-      username: {
-        type: DataTypes.STRING,
-        allowNull: false
-      },
-      password: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-          len: [4]
-        }
-      }
+User.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true
   },
-  //hooks go here
-  // hooks: {
-  // async functions for hashing password
-  // async function for hashing updated password
-  //},
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'user'
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      len: [4]
+    }
   }
-);
+}, {
+  hooks: {
+    async beforeCreate(newUserData) {
+      newUserData.password = await bcrypt.hash(newUserData.password, 10)
+      return newUserData;
+    }
+  },
+  sequelize,
+  timestamps: false,
+  freezeTableName: true,
+  underscored: true,
+  modelName: 'user'
+});
 
 module.exports = User;
