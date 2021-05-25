dd3c2aabf3a831aee7d1fc9cb2c84b39acd5ff3b
Lacey <lacey.griffith@g.austincc.edu>
2021-05-25 11:34:04 -0500

    add target group routes

--- controllers/api/target-group-routes.js
+++ controllers/api/target-group-routes.js
@@ -7,27 +7,61 @@ router.get('/', (req, res) => {
 
 }).then(targetData => res.json(targetData))
 .catch(err => res.status(500).json(err))
-})
+});
 
 //get target group by id
-// router.get('/:id', (req,res) => {
-//     TargetGroup.findOne({
-
-//     })
-// })
-
-//update target group
-// router.put('/:id', (req, res) => {
-
-// })
+router.get('/:id', (req,res) => {
+    TargetGroup.findOne({
+        where: {
+            id: req.params.id
+        }
+}).then(targetData => {
+    if(!targetData){
+        res.status(404).json({message: 'Target group not found.'})
+        return
+    }
+    res.json(targetData)
+    }).catch(err => res.status(500).json(err))
+});
 
 //create new target group
-// router.post('/', (req, res) => {
+router.post('/', (req, res) => {
+    TargetGroup.create({
+        target_group: req.body.target_group
+    }).then(targetData => res.json(targetData))
+    .catch(err => res.status(500).json(err))
+});
 
-// })
+//update target group
+router.put('/:id', (req, res) => {
+    TargetGroup.update(
+        {target_group: req.body.target_group}, {
+        where: {
+            id: req.params.id
+        }
+    }).then(targetData => {
+        if(!targetData){
+            res.status(404).json({message: 'Target group not found.'})
+            return
+        }
+        res.json(targetData)
+    }).catch(err => res.status(500).json(err))
+});
+
 
 //delete target group
-// router.delete('/:id', (req, res) => {
+router.delete('/:id', (req, res) => {
+    TargetGroup.destroy({
+        where: {
+            id: req.params.id
+        }
+    }).then(targetData => {
+        if(!targetData){
+            res.status(404).json({message: 'Target group not found.'})
+            return
+        }
+        res.json(targetData)
+    }).catch(err => res.status(500).json(err))
+});
 
-// })
 module.exports = router;
