74801ec874c860a34da8be052e5f89b3ab1aff9c
Fernanda Frers <fernanda.frers@gmail.com>
2021-05-26 17:56:31 -0500

    implementing passport

--- controllers/index.js
+++ controllers/index.js
@@ -1,10 +1,14 @@
 const router = require('express').Router();
-
+const passport = require('passport')
+const session = require('express-session')
 const apiRoutes = require('./api');
 const loginRoute = require('./home-routes')
 
+router.use(session({ secret: "namaste a while", resave: true, saveUninitialized: true }))
+router.use(passport.initialize());
+router.use(passport.session());
 router.use('/api', apiRoutes);
-router.use('/', loginRoute)
+router.use('/', loginRoute);
 
 router.use((req, res) => {
   res.status(404).end();
