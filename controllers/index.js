ad864fc70c8a363a1445153b1ebcebc9a5c43194
Lacey <lacey.griffith@g.austincc.edu>
2021-05-25 20:48:27 -0500

    connect templategit add .

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 
 const apiRoutes = require('./api');
+const loginRoute = require('./home-routes')
 
 router.use('/api', apiRoutes);
+router.use('/', loginRoute)
 
 router.use((req, res) => {
   res.status(404).end();
