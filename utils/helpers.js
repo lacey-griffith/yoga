ea1526dd77d1481f5e7d7f769d3892481f802748
Fernanda Frers <fernanda.frers@gmail.com>
2021-05-25 21:41:09 -0500

    helpers format date

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,3 +1,6 @@
-//module.exports = {}
-//format dates for comments
+module.exports = {
+    format_date: date => {
+        return`${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(date).getFullYear()}`
+    }
+}
 //format anything else that needs formatting :)
