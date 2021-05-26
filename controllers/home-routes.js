db03a683e3a26ffdc872377ada87cf3de941c905
Kyle <kj.mckinley@gmail.com>
2021-05-25 21:07:28 -0500

    making certain all code is identical to the repo

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,7 +3,9 @@ const sequelize = require('../config/connection');
 //const isLoggedIn = require('../utils/auth')
 const {User} = require('../models');
 
+
 router.get('/', (req, res) => {
     res.render('login')
 });
+
 module.exports = router;
