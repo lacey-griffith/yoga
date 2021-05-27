48d05bc9c420161998d871edd2ac43f67fb46520
Samantha Arellano <Sambollano54@gmail.com>
2021-05-26 22:18:02 -0500

    .

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -7,5 +7,7 @@ const {User} = require('../models');
 router.get('/', (req, res) => {
     res.render('login')
 });
-
+router.get('/home', (req, res) => {
+    res.render('home')
+});
 module.exports = router;
--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const express = require('express');
 const routes = require('./controllers/');
 const exphbs = require('express-handlebars');
 const sequelize = require('./config/connection');
-const helpers = require('./utils/helpers')
-
+const helpers = require('./utils/helpers');
+const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -13,7 +13,7 @@ app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use(express.static(path.join(__dirname, "public")));
 // turn on routes
 app.use(routes);
 app.get("/", ((req, res) => {
