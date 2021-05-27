874fc482d067a156669c2051b2e1932b9593a65c
Samantha Arellano <Sambollano54@gmail.com>
2021-05-26 22:54:04 -0500

    .

--- server.js
+++ server.js
@@ -2,11 +2,9 @@ const express = require('express');
 const routes = require('./controllers/');
 const exphbs = require('express-handlebars');
 const sequelize = require('./config/connection');
-<<<<<<< HEAD
-const helpers = require('./utils/helpers');
-=======
+
 const helpers = require('./utils/helpers')
->>>>>>> develop
+
 const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -30,19 +28,12 @@ app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-<<<<<<< HEAD
-app.use(express.static(path.join(__dirname, "public")));
-=======
+
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(session(sess))
 
->>>>>>> develop
 // turn on routes
 app.use(routes);
-app.get("/", ((req, res) => {
-// res.render("home")
-res.send("hello")
-}))
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening On PORT ' + PORT));
