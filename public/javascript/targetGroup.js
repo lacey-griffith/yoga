9a01bb5db48f61e149a56916d4004f9fa8a8ec3c
Lacey <lacey.griffith@g.austincc.edu>
2021-05-27 17:23:56 -0500

    correct data  collected, needs to render

--- public/javascript/targetGroup.js
+++ public/javascript/targetGroup.js
@@ -1,26 +1,39 @@
- async function findTargetGroup(event) {
+//  async function findTargetGroup(event) {
+//     event.preventDefault();
+
+
+//     const target = document.querySelector('#dropdown-item-text').innerText
+//     console.log('=====')
+//     console.log(target)
+//     console.log('=====')
+//     const response = await fetch('/api/targetgroups', {
+//         method: 'post',
+//         body: JSON.stringify({
+//             target
+
+//         }),
+//         headers: {
+//             'Content-Type': 'application/json'
+//           }
+//     }).then(res => {
+
+//         console.log(res, 'line 19')
+//         console.log(target, 'line 20')
+//     })
+// }
+
+$('#dropdown-list li a').on('click', function(){
     event.preventDefault();
-    
-    const target = document.querySelector('#dropdown-item-text').innerText
-    console.log('=====')
+    //store value of selected target group (ex: Abs) into target variable
+    const target = $(this).text();
+
+    //set queryURL
+    const query = '/api/targetgroups'
     console.log(target)
-    console.log('=====')
-    const response = await fetch('/api/targetgroups', {
-        method: 'post',
-        body: JSON.stringify({
-            target
 
-        }),
-        headers: {
-            'Content-Type': 'application/json'
-          }
+    fetch(query)
     }).then(res => {
+        console.log(res, 'line 36')
+});
 
-        console.log(res, 'line 19')
-        console.log(target, 'line 20')
-    })
-
-
-}
-
-document.querySelector('.dropdown-menu').addEventListener('click', findTargetGroup);
+//document.querySelector('.dropdown-menu').addEventListener('click', findTargetGroup);
