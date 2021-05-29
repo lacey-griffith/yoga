92a6a5556852208a3b8c98272ce746515b24da8f
Lacey <lacey.griffith@g.austincc.edu>
2021-05-28 19:18:02 -0500

    trying to render

--- public/javascript/targetGroup.js
+++ public/javascript/targetGroup.js
@@ -1,6 +1,5 @@
 
-
-$('#dropdown-list li a').on('click', function(){
+$('#dropdown-list li a').on('click', function () {
     event.preventDefault();
     //store value of selected target group (ex: Abs) into target variable
     const target_group_id = $(this).attr('id')
@@ -8,26 +7,15 @@ $('#dropdown-list li a').on('click', function(){
     //console.log(value)
 
     fetch(`/api/targetgroups/${target_group_id}`)
-    .then(res => {
-        return res.json();
-        //console.log(results)
-        //findPoses(results);
-    }).then(res => {
-        console.log(res)
-    })
+        .then(res => {
+            return res.json();
+            //console.log(results)
+            //findPoses(results);
+        }).then(res => {
+            const poses = res.poses
+            printPoses(poses)
+        })
 });
 
-// function findPoses(results) {
-//     const selectedTarget_id = results.id
-//     const selectedTarget_name = results.target_group
-
-//     fetch(`/api/poses`)
-//     .then(res => {
-//         return res.json();
-//     }).then(results => {
-//         console.log(results)
-//     })
-    
-// }
 
 //document.querySelector('.dropdown-menu').addEventListener('click', findTargetGroup);
