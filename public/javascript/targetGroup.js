04ff6ee6d06e7a576a5d49c2fc2800e858eb0e4f
Fernanda Frers <fernanda.frers@gmail.com>
2021-05-28 19:52:03 -0500

    check back on develop

--- public/javascript/targetGroup.js
+++ public/javascript/targetGroup.js
@@ -1,27 +1,33 @@
+renderYogaPoses = (id) => {
+    fetch(`/api/targetgroups/${target_group.id}`).then(res => {
+        console.log(res.json)
+        return res.json()
+    }).then(function (poses) {
+        for (let i = 0; i < poses.length; i++) {
+            let card = `
+        <div class='col-4 mt-3'>
+        <div class="card results-card" style="width: 18rem;">
+          <img src="/images/lotusbehappy.png" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Pose Name:${poses[i].pose_name}</h5>
+            <p class="card-text">Move slowly through each pose, remembering to breathe as you move. Pause after any pose you find challenging, especially if you are short of breath, and start again when your breathing returns to normal. The idea is to hold each pose for a few, slow breaths before moving on to the next one.</p>
+          </div>
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item">Difficulty: ${poses[i].difficulty}</li>
+            <li class="list-group-item">Targeted anatomy:${poses[i].target_group}</li>
+          </ul>
+          <div class="card-body">
+            <a href="https://www.youtube.com/results?search_query=${poses[i].pose_name}" class="card-link">Video Demo</a>
+          </div>
+        </div>
+        </div>
+    `
+    $(".poses").append(card)
+    }
+  })
+}
 
-
-$('#dropdown-list li a').on('click', function () {
-    event.preventDefault();
-    //store value of selected target group (ex: Abs) into target variable
-    const target_group_id = $(this).attr('id')
-    const target_group = $(this).text().trim();
-    //console.log(value)
-
-    fetch(`/api/targetgroups/${target_group_id}`)
-    .then(res => {
-        return res.json();
-    }).then(res => {
-        document.location.replace('/dashboard')
-    })
-        .then(res => {
-            return res.json();
-            //console.log(results)
-            //findPoses(results);
-        }).then(res => {
-            const poses = res.poses
-            printPoses(poses)
-        })
-});
-
-
-//document.querySelector('.dropdown-menu').addEventListener('click', findTargetGroup);
+$('#dropdown-list li a').on('click', function(event) {
+    event.preventDefault()
+    renderYogaPoses($(this).attr('id'))
+})
