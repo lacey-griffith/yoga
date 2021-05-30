2f11882295cbfaebf3b02e66edd28614a5fe767e
Fernanda Frers <fernanda.frers@gmail.com>
2021-05-30 13:10:43 -0500

    copied routes from developed to test why code is bugged

--- controllers/api/target-group-routes.js
+++ controllers/api/target-group-routes.js
@@ -16,6 +16,32 @@ router.get('/', (req, res) => {
 });
 
 //get target group by id
+router.get('/:id', (req,res) => {
+    TargetGroup.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: ['id','target_group'],
+        include: {
+            model: Pose,
+            attributes: ['id','pose_name','difficulty','target_group_id']
+        }
+}).then(targetData => {
+    if(!targetData){
+        res.status(404).json({message: 'Target group not found.'})
+        return
+    }
+    res.json(targetData)
+    }).catch(err => res.status(500).json(err))
+});
+
+//create new target group
+// router.post('/', (req, res) => {
+//     TargetGroup.create({
+//         target_group: req.body.target_group
+//     }).then(targetData => res.json(targetData))
+//     .catch(err => res.status(500).json(err))
+// });
 
 router.post('/card', (req, res) => {
     TargetGroup.findAll({
