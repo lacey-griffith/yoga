9a890e13bc8943cf5a3f5bbf44fd99c29638b6e0
Lacey <lacey.griffith@g.austincc.edu>
2021-05-30 11:54:29 -0500

    clear old search results when new search is performed

--- public/javascript/targetGroup.js
+++ public/javascript/targetGroup.js
@@ -1,9 +1,10 @@
 
 $('#dropdown-list li a').on('click', function () {
-    event.preventDefault();
 
+    event.preventDefault();
+    $('#pose_container').html('')
     const target_group_id = $(this).attr('id')
-    //const target_group = $(this).text().trim();
+    const target_group = $(this).text().trim();
 
     fetch(`/api/targetgroups/${target_group_id}`)
         .then(res => {
@@ -11,9 +12,9 @@ $('#dropdown-list li a').on('click', function () {
         }).then(res => {
             const poses = res.poses
 
-            console.log(poses[0])
-            console.log(target_group_id, 'selected target group id')
-            printPoses(poses)
+            // console.log(poses[0])
+            // console.log(target_group_id, 'selected target group id')
+            printPoses(poses, target_group)
             //return poses[0];
         })
     })
@@ -22,22 +23,26 @@ $('#dropdown-list li a').on('click', function () {
 //     return poses;
 // }
 
-function printPoses(poses) {
-
-    console.log(poses)
+function printPoses(poses, target_group) {
+    console.log(target_group, 'target group')
+    console.log(poses, 'poses')
+    
     for (let i = 0; i < poses.length; i++) {
-        console.log(poses[i].pose_name)
+      if(!poses.length){
+        $('#pose_container').append(`
+        <p>No poses were found</p>`)
+      } else {
     $('#pose_container').append(`
     <div class='col-4 mt-3'>
     <div class="card results-card" style="width: 18rem;">
       <img src="/images/lotusbehappy.png" class="card-img-top" alt="...">
       <div class="card-body">
-        <h5 class="card-title">Pose Name:${poses[i].pose_name}</h5>
+        <h5 class="card-title">${poses[i].pose_name}</h5>
         <p class="card-text">Move slowly through each pose, remembering to breathe as you move. Pause after any pose you find challenging, especially if you are short of breath, and start again when your breathing returns to normal. The idea is to hold each pose for a few, slow breaths before moving on to the next one.</p>
       </div>
       <ul class="list-group list-group-flush">
         <li class="list-group-item">Difficulty: ${poses[i].difficulty}</li>
-        <li class="list-group-item">Targeted anatomy:${poses[i].target_group}</li>
+        <li class="list-group-item">Targeted anatomy: ${target_group}</li>
       </ul>
       <div class="card-body">
         <a href="https://www.youtube.com/results?search_query=${poses[i].pose_name}" class="card-link">Video Demo</a>
@@ -45,5 +50,6 @@ function printPoses(poses) {
     </div>
     </div>
 `)
+      }
     }
 }
